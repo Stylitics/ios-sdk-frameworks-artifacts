84ddb9dc352865ad25116f95eda6b025154cfde5
Tushar Mandhare <tusharmand@cybage.com>
2023-08-30 15:00:32 +0530

    Updated UX SDK and Data SDK frameworks

--- StyliticsUI.xcframework/ios-arm64_x86_64-simulator/StyliticsUI.framework/Headers/StyliticsUI.h
+++ StyliticsUI.xcframework/ios-arm64_x86_64-simulator/StyliticsUI.framework/Headers/StyliticsUI.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double StyliticsUIVersionNumber;
 FOUNDATION_EXPORT const unsigned char StyliticsUIVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <StyliticsUI/PublicHeader.h>
-#import <StyliticsUI/StyliticsData-Swift.h>
-
